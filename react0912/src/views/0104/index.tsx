8194a72749c5c80223563d8af085c18583c104e5
yymim <yymim8080@gmail.com>
2024-01-04 17:18:01 +0900

    0104

---
+++ react0912/src/views/0104/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import UseReducer01 from "./a_useReducer/UseReducer01";
+import UseReducer02 from "./a_useReducer/UseReducer02";
+import CustomHook01 from "./b_customHook/CustomHook01";
+import Framework from "./c_style_framework/Framework";
+
+export default function Index() {
+  return (
+    <>
+      <h1>20240104 수업 자료</h1>
+      <h2>useReducer</h2>
+      <UseReducer01 />
+      <UseReducer02 />
+
+      <h2>Custom Hook</h2>
+      <CustomHook01 />
+
+      <h2>리액트 CSS 프레임워크</h2>
+      <Framework />
+    </>
+  );
+}
