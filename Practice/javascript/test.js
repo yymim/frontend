d7ccbd553d77197407e405c0aaca071946c295bd
yymim <yymim8080@gmail.com>
2024-01-25 23:57:10 +0900

    0125

--- Practice/javascript/test.js
+++ Practice/javascript/test.js
@@ -1,7 +1,31 @@
 {
   // 스크립트 시작
 
+  let a =[];
+  a.sample = 10;
+
+  console.log(a.sample);
+  console.log(typeof a);
+
+  function b() {
+  };
+  b.sample = 10;
+
+  console.log(b.sample);
+  console.log(typeof b);
+
+  console.log(Array.isArray(a));
+
   
+  const f = new Number(273);
+  const g = Number(273);
+  
+  console.log(typeof f);
+  console.log(typeof g);
+
+  f.sample = 10;
+  console.log(f.sample);
+  console.log(f);
 
   // 스크립트 종료
 };
