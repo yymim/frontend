ef6062f91d343cc8232cd24b7a3e0514f661053f
yymim <yymim@ezinc.co.kr>
2025-08-18 19:24:47 +0900

    0818 section08

--- ONEBITE-REACT/section08/src/components/List.jsx
+++ ONEBITE-REACT/section08/src/components/List.jsx
@@ -1,18 +1,48 @@
 import './List.css';
-import TodoItem from './TotoItem'
+import TodoItem from './TodoItem'
+import { useState } from 'react';
 
-const List = ({ todos }) => {
+const List = ({ todos, onUpdate, onDelete }) => {
+
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value)
+  };
+
+  const getFilteredData = () => {
+    if(search === '') {
+      return todos;
+    } 
+    return todos.filter((todo) => 
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className='List'>
       <h4>Todo List🌸</h4>
-      <input placeholder='검색어를 입력하세요' />
+      <input
+        value = {search}
+        onChange = {onChangeSearch}
+        placeholder='검색어를 입력하세요' 
+      />
       <div className='todos_wrapper'>
         {/* todos에 배열 메서드인 map 메서드 이용 */}
         {/* {todos.map((todo) => {
           return <div>{todo.content}</div>;
         })}  */}
-        {todos.map((todo) => {
-          return <TodoItem {...todo} />;
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
         })} 
       </div>
     </div>
