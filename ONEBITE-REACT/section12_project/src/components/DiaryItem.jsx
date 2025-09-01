464fac2e1819a39a52502b6d7cf49cb33f96b148
yymim <yymim@ezinc.co.kr>
2025-09-01 20:58:59 +0900

    0901_home

--- ONEBITE-REACT/section12_project/src/components/DiaryItem.jsx
+++ ONEBITE-REACT/section12_project/src/components/DiaryItem.jsx
@@ -1,26 +1,36 @@
 import './DiaryItem.css'
 import { getEmotionImage } from '../util/get-emotion-image.js';
 import Button from './Button.jsx';
+import { useNavigate } from 'react-router-dom';
 
-const DiaryItem = () => {
+const DiaryItem = ({id, emotionId, createdDate, content}) => {
 
-  const emotionId = 1;
+  const nav = useNavigate();
 
   return (
     <div className='DiaryItem'>
-      <div className={`img_section img_section_${emotionId}`}>
-        <img src={getEmotionImage(1)}/>
+      <div 
+        onClick={()=>nav(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
+        <img src={getEmotionImage(id)}/>
       </div>
-      <div className='info_section'>
+      <div 
+        onClick={()=>nav(`/diary/${id}`)}
+        className='info_section'
+      >
         <div className='created_date'>
-          {new Date().toLocaleDateString()}
+          {new Date(createdDate).toLocaleDateString()}
         </div>
         <div className='content'>
-          일기 컨텐츠
+          {content}
         </div>
       </div>
       <div className='button_section'>
-        <Button text={'수정하기'} />
+        <Button 
+          onClick={()=>nav(`/edit/${id}`)}
+          text={'수정하기'}
+        />
       </div>
     </div>
   );
--- ONEBITE-REACT/section12_project/src/pages/Home.jsx
+++ ONEBITE-REACT/section12_project/src/pages/Home.jsx
@@ -1,16 +1,63 @@
+import { useState, useContext } from 'react';
+import { DiaryStateContext } from '../App';
+
 import Header from '../components/Header';
 import Button from '../components/Button';
 import DiaryList from '../components/DiaryList';
 
 
+const getMonthlyDate = (pivotDate, data) => {
+
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1, // 일
+    0, // 시
+    0, // 분
+    0  // 초
+  ).getTime();
+
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0, // 이전달의 마지막 날에 해당하는 숫자로 변환
+    23,
+    59,
+    59
+  ).getTime();
+
+
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+}
+
+
 const Home = () => {
+  const data = useContext(DiaryStateContext);
+  const [ pivotDate, setPivotDate ] = useState(new Date());
+
+  const monthlyData = getMonthlyDate(pivotDate, data);
+  
+  const onIncreaseMonth = () => {
+    setPivotDate(
+      new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
+    );
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(
+      new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
+    );
+  };
+
   return (
     <div>
-      <Header title={'2025년 9월'}
-        leftChild={<Button text={'<'} />}
-        rightChild={<Button text={'>'} />}
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`} // getMonth -> +1 필요
+        leftChild={<Button onClick={onDecreaseMonth} text={'<'} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={'>'} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </div>
   );
 }
