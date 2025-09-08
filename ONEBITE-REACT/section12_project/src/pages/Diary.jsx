ad80b7658bd72aab9bdf1a710d8bf57bb5759cc6
yymim <yymim@ezinc.co.kr>
2025-09-08 21:23:09 +0900

    0908

--- ONEBITE-REACT/section12_project/src/pages/Diary.jsx
+++ ONEBITE-REACT/section12_project/src/pages/Diary.jsx
@@ -1,10 +1,50 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Viewer from '../components/Viewer';
+import useDiary from '../hooks/useDiary';
+import { getStringedDate } from "../util/get-stringed-date";
 
 const Diary = () => {
   const params = useParams();
-  console.log(params);
+  const nav = useNavigate();
 
-  return <div>{params.id}번 일기입니다 ~~</div>;
+  const curDiaryItem = useDiary(params.id);
+
+  if( !curDiaryItem ) {
+    return (
+      <div>
+        데이터 로딩중...!
+      </div>
+    );
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+
+  return (
+    <div>
+      <Header 
+        title={`${title} 기록`}
+        leftChild={
+          <Button 
+            onClick={() => nav(-1)}
+            text={'< 뒤로가기'}
+          />
+        }
+        rightChild={
+          <Button 
+            onClick={() => nav(`/edit/${params.id}`)}
+            text={'수정하기'}
+          />
+        }
+      />
+      <Viewer
+        emotionId={emotionId}
+        content={content}
+      />
+    </div>
+  );
 };
 
 export default Diary;
--- ONEBITE-REACT/section12_project/src/pages/Edit.jsx
+++ ONEBITE-REACT/section12_project/src/pages/Edit.jsx
@@ -2,30 +2,17 @@ import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
-import { useContext, useEffect, useState } from "react";
-import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import useDiary from '../hooks/useDiary';
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
 
-  const data = useContext(DiaryStateContext);
-  const [curDiaryItem, setCurDiaryItem] = useState();
-
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    );
-
-    if (!currentDiaryItem) {
-      window.alert("존재하지 않는 일기입니다.");
-      nav("/", { replace: true });
-    }
-
-    setCurDiaryItem(currentDiaryItem);
-  }, [params.id]);
-
+  const curDiaryItem = useDiary(params.id);
+  
   const onClickDelete = () => {
     if (
       window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")
